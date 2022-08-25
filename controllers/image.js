e76d8c009ddc8a48dfeab32dc063c8f7e4400a56
febomtempo <67077931+febomtempo@users.noreply.github.com>
2022-08-25 12:50:52 -0300

    Update image.js
    link da img

--- controllers/image.js
+++ controllers/image.js
@@ -5,10 +5,13 @@ const app = new Clarifai.App({
   apiKey: process.env.CLARIFAI_API,
 });
 
+let imgUrl;
+
 const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then((data) => {
+      imgUrl = req.body.input;
       res.json(data);
     })
     .catch((err) =>
@@ -17,13 +20,19 @@ const handleApiCall = (req, res) => {
 };
 
 const handleImage = (req, res, db) => {
-  const { id } = req.body;
+  const { id, input } = req.body;
   db("users")
     .where("id", "=", id)
     .increment("entries", 1)
     .returning("entries")
     .then((entries) => {
       res.json(entries[0].entries);
+      db.insert({ user_id: req.body.id, link: imgUrl })
+        .into("fotos")
+        .then((data) => {
+          console.log("sucesso");
+        })
+        .catch((err) => console.log(err));
     })
     .catch((err) => res.status(400).json("Não foi possível pegar as entries"));
 };
@@ -32,3 +41,4 @@ module.exports = {
   handleImage,
   handleApiCall,
 };
+
