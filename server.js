c38640954f47fe3093608a43054abb60755067be
febomtempo <febomtempo@hotmail.com>
2022-08-25 05:49:23 -0300

    config db

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   },
 });
 
